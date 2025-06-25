483a5ea053fdda0d4b9b770e495dc211868dae91
Tharunethu <tharunethu03@icloud.com>
2025-06-25 05:35:39 +0530

    Implement cocktails section

--- constants/index.jsx
+++ constants/index.jsx
@@ -21,13 +21,13 @@ const cocktailLists = [
   {
     name: "Chapel Hill Shiraz",
     country: "AU",
-    detail: "Battle",
+    detail: "Bottle",
     price: "$10",
   },
   {
     name: "Caten Malbee",
     country: "AU",
-    detail: "Battle",
+    detail: "Bottle",
     price: "$49",
   },
   {
